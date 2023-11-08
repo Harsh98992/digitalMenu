251ddd607994c2a9b26057ca0a74f095ac9041e1
himanshu <himanshu991139@gmail.com>
2023-11-08 18:34:54 +0530

    search changes

--- src/app/admin/panel/dishes/view-dish/view-dish.component.ts
+++ src/app/admin/panel/dishes/view-dish/view-dish.component.ts
@@ -1,102 +1,118 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { ColumnMode } from '@swimlane/ngx-datatable';
-import { Subject, takeUntil } from 'rxjs';
-import { ConfirmDialogComponent } from 'src/app/angular-material/confirm-dialog/confirm-dialog.component';
-import { RestaurantPanelService } from 'src/app/api/restaurant-panel.service';
+import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { ColumnMode, DatatableComponent } from "@swimlane/ngx-datatable";
+import { Subject, takeUntil } from "rxjs";
+import { ConfirmDialogComponent } from "src/app/angular-material/confirm-dialog/confirm-dialog.component";
+import { RestaurantPanelService } from "src/app/api/restaurant-panel.service";
 
 @Component({
-  selector: 'app-view-dish',
-  templateUrl: './view-dish.component.html',
-  styleUrls: ['./view-dish.component.scss'],
+    selector: "app-view-dish",
+    templateUrl: "./view-dish.component.html",
+    styleUrls: ["./view-dish.component.scss"],
 })
 export class ViewDishComponent implements OnInit {
-  @ViewChild('editTmpl', { static: true }) editTmpl: TemplateRef<any>;
-  rows = [];
-  columns = [];
-  ColumnMode = ColumnMode;
-  destroy$: Subject<boolean> = new Subject<boolean>();
-  constructor(
-    private restaurantService: RestaurantPanelService,
-    private router: Router,
-    public dialog: MatDialog
-  ) {}
+    @ViewChild("editTmpl", { static: true }) editTmpl: TemplateRef<any>;
+    @ViewChild("myTable", { static: false }) table: DatatableComponent;
+    rows = [];
+    columns = [];
+    ColumnMode = ColumnMode;
+    destroy$: Subject<boolean> = new Subject<boolean>();
+    searchTerm = "";
+    filteredData: any;
+    constructor(
+        private restaurantService: RestaurantPanelService,
+        private router: Router,
+        public dialog: MatDialog
+    ) {}
 
-  ngOnInit(): void {
-    this.getOrders();
-    this.columns = [
-      { name: 'Actions', prop: 'action' },
-      { name: 'Dish Image', prop: 'imageUrl' },
-      { name: 'Dish Name', prop: 'dishName' },
-      { name: 'Price', prop: 'dishPrice' },
-      { name: 'Dish Type', prop: 'dishType' },
-      { name: 'Availability', prop: 'availableFlag' },
-      { name: 'Dish Order Option', prop: 'dishOrderOption' },
+    ngOnInit(): void {
+        this.getOrders();
+        this.columns = [
+            { name: "Actions", prop: "action" },
+            { name: "Dish Image", prop: "imageUrl" },
+            { name: "Dish Name", prop: "dishName" },
+            { name: "Price", prop: "dishPrice" },
+            { name: "Dish Type", prop: "dishType" },
+            { name: "Availability", prop: "availableFlag" },
+            { name: "Dish Order Option", prop: "dishOrderOption" },
 
-      { name: 'Dish Description', prop: 'dishDescription' },
-      { name: 'Spicy', prop: 'chilliFlag' },
-    ];
-  }
-  getOrders() {
-    this.restaurantService.restaurantData
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (res: any) => {
-          if (res && res.cuisine) {
-            const dishes = this.getAllDishes(res?.cuisine);
-
-            this.rows = dishes;
-          }
-        },
-      });
-  }
-  navigateToEditDish(row: any) {
-    this.restaurantService.setSelectedDish(row);
-    this.router.navigate(['/admin/dishes/add-dish'], {
-      queryParams: { edit: true },
-    });
-  }
-  getAllDishes(cuisine) {
-    const result = [];
-    if (cuisine) {
-      for (let dish of cuisine) {
-        if (dish['items'] && dish['items'].length) {
-          console.log(dish['items']);
-          const newArray = dish['items'].map((obj) => ({
-            ...obj,
-            categoryId: dish['_id'],
-          }));
-          result.push(...newArray);
-        }
-      }
+            { name: "Dish Description", prop: "dishDescription" },
+            { name: "Spicy", prop: "chilliFlag" },
+        ];
     }
-    return result;
-  }
-  deleteDish(row: any) {
-    const dialogData = {
-      title: 'Confirm',
-      message: 'Are you sure you want to delete this item?',
-    };
-    this.dialog
-      .open(ConfirmDialogComponent, { data: dialogData })
-      .afterClosed()
-      .subscribe({
-        next: (res: any) => {
-          if (res && res.okFlag) {
-            this.restaurantService
-              .deleteDish(row._id, { categoryId: row.categoryId })
-              .subscribe({
-                next: (res) => {
-                  this.restaurantService.setRestaurantData(res);
-                },
-              });
-          }
-        },
-      });
-  }
+    getOrders() {
+        this.restaurantService.restaurantData
+            .pipe(takeUntil(this.destroy$))
+            .subscribe({
+                next: (res: any) => {
+                    if (res && res.cuisine) {
+                        const dishes = this.getAllDishes(res?.cuisine);
 
-  // navigateToOrderView(row) {
-  //   this.router.navigateByUrl('/admin/restaurant-orders/view');
-  // }
+                        this.rows = dishes;
+                        this.applyFilter();
+                    }
+                },
+            });
+    }
+    applyFilter(): void {
+        const filterValue = this.searchTerm.toLowerCase();
+
+        this.filteredData = this.rows.filter((row) => {
+            return Object.keys(row).some((key) =>
+                String(row[key]).toLowerCase().includes(filterValue)
+            );
+        });
+        this.table.offset = 0; // Reset pagination to the first page
+    }
+    navigateToEditDish(row: any) {
+        this.restaurantService.setSelectedDish(row);
+        this.router.navigate(["/admin/dishes/add-dish"], {
+            queryParams: { edit: true },
+        });
+    }
+    getAllDishes(cuisine) {
+        const result = [];
+        if (cuisine) {
+            for (let dish of cuisine) {
+                if (dish["items"] && dish["items"].length) {
+                    console.log(dish["items"]);
+                    const newArray = dish["items"].map((obj) => ({
+                        ...obj,
+                        categoryId: dish["_id"],
+                    }));
+                    result.push(...newArray);
+                }
+            }
+        }
+        return result;
+    }
+    deleteDish(row: any) {
+        const dialogData = {
+            title: "Confirm",
+            message: "Are you sure you want to delete this item?",
+        };
+        this.dialog
+            .open(ConfirmDialogComponent, { data: dialogData })
+            .afterClosed()
+            .subscribe({
+                next: (res: any) => {
+                    if (res && res.okFlag) {
+                        this.restaurantService
+                            .deleteDish(row._id, { categoryId: row.categoryId })
+                            .subscribe({
+                                next: (res) => {
+                                    this.restaurantService.setRestaurantData(
+                                        res
+                                    );
+                                },
+                            });
+                    }
+                },
+            });
+    }
+
+    // navigateToOrderView(row) {
+    //   this.router.navigateByUrl('/admin/restaurant-orders/view');
+    // }
 }

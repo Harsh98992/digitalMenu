fb6affe4cb7b614fc2f72cd863b9e2a312f92cb0
Chirag <76880977+chirag127@users.noreply.github.com>
2023-12-13 17:49:53 +0530

    Fix image validation and update variant label

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,9 @@
 export const environment = {
     production: false,
-    apiUrl: "https://qrsay-backend-testing.onrender.com/api",
-    socketApiUrl: "https://qrsay-backend-testing.onrender.com/",
+    // apiUrl: "https://qrsay-backend-testing.onrender.com/api",
+    // socketApiUrl: "https://qrsay-backend-testing.onrender.com/",
 
 
-    // apiUrl: "http://localhost:8080/api",
-    // socketApiUrl: "http://localhost:8080/",
-
+    apiUrl: "http://localhost:8080/api",
+    socketApiUrl: "http://localhost:8080/",
 };

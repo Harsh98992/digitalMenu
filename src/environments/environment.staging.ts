abbd8ef7338360021dd313c618a4ba7057909d67
himanshu <himanshu991139@gmail.com>
2024-02-03 15:29:26 +0530

    staging env changes

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: "https://qrsay-backend-testing.onrender.com/api",
+    socketApiUrl: "https://qrsay-backend-testing.onrender.com/",
+};

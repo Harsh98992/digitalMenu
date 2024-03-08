232bd973521220a35d2d8a11df3defce5cb96ba9
himanshu <himanshu991139@gmail.com>
2024-03-08 23:33:16 +0530

    bug fix

--- src/app/restaurant/component/restaurant-menu/table-number-dialog/table-number-dialog.component.ts
+++ src/app/restaurant/component/restaurant-menu/table-number-dialog/table-number-dialog.component.ts
@@ -1,7 +1,9 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { CustomerService } from "src/app/api/customer.service";
 import { UtilService } from "src/app/api/util.service";
+import { CustomerAuthService } from "src/app/restaurant/api/customer-auth.service";
 import { RestaurantService } from "src/app/restaurant/api/restaurant.service";
 
 @Component({
@@ -14,17 +16,30 @@ export class TableNumberDialogComponent implements OnInit {
     restaurantData: any;
     tableData: any;
     selectedTable: any;
+    customerDetailId: any;
     constructor(
         private fb: FormBuilder,
         private restaurnatService: RestaurantService,
         @Inject(MAT_DIALOG_DATA) public dialogData: any,
         public dialogRef: MatDialogRef<TableNumberDialogComponent>,
-        private utilityService: UtilService
+        private utilityService: UtilService,
+        private customerAuthService: CustomerAuthService
     ) {}
 
     ngOnInit(): void {
         this.restaurantData = this.dialogData.restaurantData;
-        this.tableData = this.dialogData.tableData;
+        
+        this.customerAuthService.customerDetail.subscribe({
+            next: (res) => {
+                if (res) {
+                    console.log(res);
+                    this.customerDetailId=res._id
+                    this.tableData = this.dialogData.tableData;
+                } else {
+                    // this.userLoginFlag = false;
+                }
+            },
+        });
         console.log(this.tableData);
     }
 

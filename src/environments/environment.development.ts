9cc371928265e6cd7fe482f6e8bbe4bac3b05a8f
Chirag <76880977+chirag127@users.noreply.github.com>
2024-04-23 16:31:40 +0530

    Update API URLs in environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,9 +5,9 @@ export const environment = {
 
     // apiUrl: "https://qrsay-backend-testing.onrender.com/api",
     // socketApiUrl: "https://qrsay-backend-testing.onrender.com/",
-    // apiUrl: "https://qrsaybackend-ksaw.onrender.com/api",
-    // socketApiUrl: "https://qrsaybackend-ksaw.onrender.com/",
+    apiUrl: "https://qrsaybackend-ksaw.onrender.com/api",
+    socketApiUrl: "https://qrsaybackend-ksaw.onrender.com/",
 
-    apiUrl: "http://localhost:8080/api",
-    socketApiUrl: "http://localhost:8080/",
+    // apiUrl: "http://localhost:8080/api",
+    // socketApiUrl: "http://localhost:8080/",
 };

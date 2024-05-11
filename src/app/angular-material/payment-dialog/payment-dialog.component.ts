eba40d66d9dfa35d7e5432ea7b3000d6c5e0a432
himanshu <himanshu991139@gmail.com>
2024-05-12 00:59:33 +0530

    bug fix

--- src/app/angular-material/payment-dialog/payment-dialog.component.ts
+++ src/app/angular-material/payment-dialog/payment-dialog.component.ts
@@ -71,7 +71,7 @@ export class PaymentDialogComponent implements OnInit {
                 "room service"
             ) {
                 this.paymentMethod = "payOnline";
-                this.paymentOption.pop();
+                this.paymentOption = this.paymentOption.filter((x) => x.key === "payOnline");
                 this.completePayment();
             }
         }

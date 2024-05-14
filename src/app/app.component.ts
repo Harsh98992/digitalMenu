a3bb33df39f035f46b530a9ee761b596e1ead881
himanshu <himanshu991139@gmail.com>
2024-05-14 15:46:56 +0530

    reload functionality applied for inactivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RestaurantService } from "./restaurant/api/restaurant.service";
 import { AuthenticationService } from "./api/authentication.service";
 import { CustomerAuthService } from "./restaurant/api/customer-auth.service";
 import { OrderService } from "./api/order.service";
+import { Subject } from "rxjs";
 
 @Component({
     selector: "app-root",
@@ -11,12 +12,40 @@ import { OrderService } from "./api/order.service";
 })
 export class AppComponent {
     title = "QrSay";
+    private idleTimer: any;
+    private readonly idleDuration = 4 * 60 * 60 * 1000; // 4 hours in milliseconds
+    public userActivity: Subject<void> = new Subject<void>();
     constructor(
         private authService: AuthenticationService,
         private customerAuthService: CustomerAuthService,
         private orderService: OrderService
     ) {
         this.checkLogin();
+        this.startIdleTimer();
+    }
+    startIdleTimer(): void {
+        this.resetIdleTimer();
+        // Listen for mouse and touch events
+        document.addEventListener("mousemove", () => this.onUserActivity());
+        document.addEventListener("touchstart", () => this.onUserActivity());
+        document.addEventListener("keydown", () => this.onUserActivity());
+        document.addEventListener("scroll", () => this.onUserActivity());
+    }
+    resetIdleTimer(): void {
+        clearTimeout(this.idleTimer);
+        this.idleTimer = setTimeout(
+            () => this.refreshPage(),
+            this.idleDuration
+        );
+    }
+
+    refreshPage(): void {
+        window.location.reload();
+    }
+
+    private onUserActivity(): void {
+        this.userActivity.next();
+        this.resetIdleTimer();
     }
     checkLogin() {
         if (this.authService.getUserToken()) {

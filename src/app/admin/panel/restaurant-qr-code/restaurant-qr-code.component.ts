f826476aaa9d367650eba2d6fc097cf9a649b0e7
himanshu <himanshu991139@gmail.com>
2024-06-01 15:12:04 +0530

    changes for qr code

--- src/app/admin/panel/restaurant-qr-code/restaurant-qr-code.component.ts
+++ src/app/admin/panel/restaurant-qr-code/restaurant-qr-code.component.ts
@@ -9,7 +9,9 @@ import { RestaurantPanelService } from "src/app/api/restaurant-panel.service";
 })
 export class RestaurantQrCodeComponent implements OnInit {
     public myAngularxQrCode: string = "";
+    public myAngularxDiningQrCode: string = "";
     public qrCodeDownloadLink: SafeUrl = "";
+    public qrCodeDiningDownloadLink: SafeUrl = "";
     constructor(private restaurantService: RestaurantPanelService) {}
 
     ngOnInit(): void {
@@ -20,11 +22,15 @@ export class RestaurantQrCodeComponent implements OnInit {
             next: (res: any) => {
                 if (res && res.restaurantUrl) {
                     this.myAngularxQrCode = `${window.location.origin}/restaurant?detail=${res.restaurantUrl}`;
+                    this.myAngularxDiningQrCode = `${window.location.origin}/restaurant?detail=${res.restaurantUrl}&dining=true`;
                 }
             },
         });
     }
     onChangeURL(url: SafeUrl) {
         this.qrCodeDownloadLink = url;
+    }
+    onChangeDiningURL(url: SafeUrl) {
+        this.qrCodeDiningDownloadLink = url;
     }
 }

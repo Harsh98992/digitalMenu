afedac0013ce2e9205b409f6678c42a38f944997
himanshu <himanshu991139@gmail.com>
2024-07-03 22:31:43 +0530

    changes for kot printintg

--- src/app/api/util.service.ts
+++ src/app/api/util.service.ts
@@ -837,6 +837,279 @@ export class UtilService {
 
         // printWindow.close();
     }
+    printKTOHelper(orderData,restaurantDetail) {
+        let orderTypeStr = "";
+        const orderDetail = _.cloneDeep(orderData);
+        if (
+            orderDetail.customerPreferences.preference.toLowerCase() ===
+            "room service"
+        ) {
+            orderTypeStr =
+                "Room Number :- " + orderDetail.customerPreferences.value;
+        } else if (
+            orderDetail.customerPreferences.preference.toLowerCase() ===
+            "delivery"
+        ) {
+            orderTypeStr =
+                "Address :- " + orderDetail.customerPreferences.value.address;
+        } else if (
+            orderDetail.customerPreferences.preference.toLowerCase() ===
+            "dine in"
+        ) {
+            orderTypeStr =
+                "Table Number :- " + orderDetail.customerPreferences.value;
+        } else if (
+            orderDetail.customerPreferences.preference.toLowerCase() ===
+            "dining"
+        ) {
+            orderTypeStr =
+                "Table Number :- " + orderDetail.customerPreferences.value;
+        } else if (
+            orderDetail.customerPreferences.preference.toLowerCase() ===
+            "take away"
+        ) {
+            orderTypeStr =
+                "Take Away :- " + orderDetail.customerPreferences.value;
+        } else if (
+            orderDetail.customerPreferences.preference.toLowerCase() ===
+            "schedule dining"
+        ) {
+            orderTypeStr =
+                "Schedule Dining :- " + orderDetail.customerPreferences.value;
+        }
+        if (
+            orderDetail?.customerPreferences?.preference?.toLowerCase() ===
+            "dine in"
+        ) {
+            for (const [index, order] of orderData.orderDetails.entries()) {
+                if (index > 0) {
+                    orderDetail.orderDetails[0]["orderSummary"].push(
+                        ...order["orderSummary"]
+                    );
+                    orderDetail.orderDetails[0]["orderAmount"] +=
+                        order["orderAmount"];
+                    orderDetail.orderDetails[0]["gstAmount"] +=
+                        order["gstAmount"];
+                    orderDetail.orderDetails[0]["deliveryAmount"] +=
+                        order["deliveryAmount"];
+                    orderDetail.orderDetails[0]["discountAmount"] +=
+                        order["discountAmount"];
+                }
+            }
+        }
+        // Please convert the above style of the bill code into the typescript code for making the print content of the bill on the print window
+
+        const printWindow = window.open("", "", "width=2in");
+
+        printWindow.document.write("<html><head><title>bill</title>");
+
+        // stylesheets
+
+        printWindow.document.write(
+            `   <style>
+            .receipt {
+                width: 300px;
+                margin: 0 auto;
+                height: 30px;
+                font-weight: 600;
+                padding: 5px;
+                font-family: Arial, sans-serif;
+                font-size: 15px;
+            }
+
+            .header {
+                text-align: center;
+            }
+
+            .header img {
+                width: 80px;
+                height: 80px;
+            }
+
+            .header h1 {
+                font-size: 20px;
+                margin: 0;
+            }
+
+            .header p {
+                margin: 2px 0;
+            }
+
+            .item-table {
+                width: 100%;
+                border-collapse: collapse;
+                margin: 5px 0;
+            }
+
+            .item-table th,
+            .item-table td {
+                border: 1px solid black;
+                padding: 2px;
+            }
+
+            .item-table th {
+                text-align: left;
+            }
+
+            .item-table td {
+                text-align: left;
+            }
+
+            .footer {
+                display: flex;
+                justify-content: space-between;
+                margin: 0px 0;
+            }
+
+            .footer p {
+                margin: 0;
+            }
+
+            .total {
+                font-weight: bold;
+            }
+            .captalize {
+                text-transform: capitalize;
+            }
+            .font-bold {
+                font-weight: bold;
+            }
+            .right {
+                text-align: right;
+            }
+            .center {
+                text-align: center !important;
+            }
+            .space-between {
+                display: flex;
+                justify-content: space-between;
+            }
+            .border-none {
+                font-size: 14px;
+                font-weight: 600;
+                border-left: 0 !important;
+                border-right: 0 !important;
+                border-bottom: 0 !important;
+                border-top: 0 !important;
+            }
+            .dash-line {
+                border-top: 2px dashed grey;
+                width: 100%;
+                margin-top: 0px;
+                margin-bottom: 0.1px;
+            }
+            span {
+                font-size: 14px;
+            }
+
+            .border-main-none {
+                font-size: 14px;
+                border-left: 0 !important;
+                border-right: 0 !important;
+                border-top: 2px dashed grey;
+                border-bottom: 2px dashed grey;
+            }
+            .margin-custom {
+                margin-right: 1.3rem !important;
+            }
+        </style>`
+        );
+        printWindow.document.write("</head><body>");
+
+        printWindow.document.write(
+            `<div class="receipt">
+            <div class="header">
+
+                <h1>${restaurantDetail.restaurantName.toUpperCase()}</h1>
+
+            </div>
+            <p style="text-align:center;margin-bottom:0px" class="captalize font-bold">${
+                orderDetail.customerPreferences.preference
+            }</p>
+            <div  style="text-align:center">${orderTypeStr}</div>
+            <p>
+            <span class="space-between">
+            ${this.datePipe.transform(orderDetail.orderDate)}
+         <span>   ${new Date(orderDetail.orderDate).toLocaleTimeString()}</span>
+            </span>
+
+            Order ID: ${orderDetail.orderId}<br>
+
+            ${orderDetail.customerName}<br>
+            ${orderDetail.customerEmail}<br>
+            <span style="word-break:break-all">
+            Cooking Instructions: ${
+                orderDetail?.orderDetails?.[0].cookingInstruction ?? ""
+            }<br>
+            </span>
+           </p>
+
+            <table class="item-table ">
+                <tr  class="border-main-none">
+                    <th  class="border-main-none">Items</th>
+
+                    <th  class="border-main-none center">Qty</th>
+                </tr>`
+        );
+
+        for (const order of orderDetail.orderDetails[0].orderSummary) {
+            printWindow.document.write("<tr  class='border-none'>");
+            printWindow.document.write(
+                `<td class='border-none'>${order.dishName}`
+            );
+
+            let orderStr = this.dishNameWithExtra(order);
+            printWindow.document.write(orderStr);
+            printWindow.document.write(
+                `<td class='border-none center'>${order.dishQuantity}</td>`
+            );
+            printWindow.document.write("</tr>");
+        }
+
+        printWindow.document.write("</table>");
+        printWindow.document.write("<div class='dash-line'></div>");
+
+        printWindow.document.write(
+            `<span>Total Quantity: ${orderDetail.orderDetails[0].orderSummary.length}</span>
+                `
+        );
+
+        // if (orderDetail.orderDetails[0].gstAmount) {
+        //     printWindow.document.write(
+        //         `<div class="footer">
+        //         <p>GST Amount</p>
+        //         <p>${orderDetail.orderDetails[0].gstAmount}</p>
+        //     </div>`
+        //     );
+        // }
+
+        printWindow.document.write("</div>");
+        printWindow.document.write("</body></html>");
+
+        console.log("printWindow", printWindow.document);
+
+        // printWindow.document.write(printContent);
+
+        var ua = navigator.userAgent.toLowerCase();
+
+        var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+
+        if (isAndroid) {
+            console.log("android");
+
+            // downlaod the file in android device with 2 inch breadth
+        } else {
+            console.log("not android");
+
+            printWindow.print();
+
+            printWindow.close();
+        }
+
+        // printWindow.print();
+
+        // printWindow.close();
+    }
     dishNameWithExtra(order) {
         let orderStr = "";
         if (order?.itemSizeSelected?.size) {

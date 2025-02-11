b786ed98ba9253e2d7c934a8bbbc73b2531edd1f
Chirag <76880977+chirag127@users.noreply.github.com>
2025-02-11 17:41:53 +0530

    Update development environment configuration to enable API and socket URLs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    // apiUrl: "https://qrsay-backend-testing.onrender.com/api",
-    // socketApiUrl: "https://qrsay-backend-testing.onrender.com/",
+    apiUrl: "https://qrsay-backend-testing.onrender.com/api",
+    socketApiUrl: "https://qrsay-backend-testing.onrender.com/",
 
     // apiUrl: "https://qrsaybackend-gurg.onrender.com/api",
     // socketApiUrl: "https://qrsaybackend-gurg.onrender.com/",
     // apiUrl: "https://qrsaybackend-ksaw.onrender.com/api",
     // socketApiUrl: "https://qrsaybackend-ksaw.onrender.com/",
-    apiUrl: "http://localhost:8080/api",
-    socketApiUrl: "http://localhost:8080/",
+    // apiUrl: "http://localhost:8080/api",
+    // socketApiUrl: "http://localhost:8080/",
 };

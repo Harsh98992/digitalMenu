2ec0967a68569bd60ea0d02508f691c907b16f8b
himanshu991139 <himanshu991139@gmail.com>
2025-02-26 17:09:23 +0530

    endpoint changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: "https://qrsaybackend.onrender.com/api",
-    socketApiUrl: "https://qrsaybackend.onrender.com/",
+    // apiUrl: "https://qrsaybackend.onrender.com/api",
+    // socketApiUrl: "https://qrsaybackend.onrender.com/",
+    apiUrl: "https://qrsaybackend-36c9.onrender.com/api",
+    socketApiUrl: "https://qrsaybackend-36c9.onrender.com/",
 };

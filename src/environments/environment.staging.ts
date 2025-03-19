0ed37137b833a2c19101ba510144b4b0d65c6577
Chirag <76880977+chirag127@users.noreply.github.com>
2025-03-19 21:07:02 +0530

    Update staging environment API and socket URLs

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,6 +2,8 @@ export const environment = {
     production: false,
     //   apiUrl: "https://qrsaybackend-gurg.onrender.com/api",
     // socketApiUrl: "https://qrsaybackend-gurg.onrender.com/",
-        apiUrl: "https://qrsay-backend-testing.onrender.com/api",
-    socketApiUrl: "https://qrsay-backend-testing.onrender.com/",
+    //     apiUrl: "https://qrsay-backend-testing.onrender.com/api",
+    // socketApiUrl: "https://qrsay-backend-testing.onrender.com/",
+    apiUrl: "https://qrsaybackend-1.onrender.com/api",
+    socketApiUrl: "https://qrsaybackend-1.onrender.com/",
 };

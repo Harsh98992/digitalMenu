959ff2d6a8e8de6b780808cdc5ab21711d7ff542
himanshu991139 <himanshu991139@gmail.com>
2025-04-30 15:48:51 +0530

    fix for order not happeing

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     //   apiUrl: "https://qrsaybackend-gurg.onrender.com/api",
     // socketApiUrl: "https://qrsaybackend-gurg.onrender.com/",
-        apiUrl: "https://qrsaybackend-testing.onrender.com/api",
-    socketApiUrl: "https://qrsaybackend-testing.onrender.com/",
+    apiUrl: "https://qrsaybackend-testing-f67e.onrender.com/api",
+    socketApiUrl: "https://qrsaybackend-testing-f67e.onrender.com/",
 };
